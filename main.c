f646c852740f1d23335ecdda5e53a32066600232
Abdelrahman.AlDesoky <abdelrahman.mohamed.aldesoky@gmail.com>
2023-10-14 16:30:55 +0300

    Edited Get Function

--- main.c
+++ main.c
@@ -24,20 +24,22 @@
 #define CONFIRM_PASSWORD 2
 
 void PrintTerminal(u8 *message,u8 enter,u8 dot);
-u8 GettingUserInputAndCheck(u8 username_or_password);
+void GettingUserInputAndCheck(u8 data[],u8 Selector);
 
-u8 password_1[100] = { 0 };
-u8 password_2[100] = { 0 };
 
 int main()
 {
 	DIO_voidSetPortDirection(PORTB_REG, PORT_DIRECTION_OUTPUT);
-
 	USART_voidInit();
 
+	u8 user_name[100] = { 0 };
+	u8 password_1[100] = { 0 };
+	u8 password_2[100] = { 0 };
+
+
 	u8 message_1[] = { "For Light on and off Press 0" };
-	u8 message_2[] = { "For user name Press 1" };
 	PrintTerminal(message_1,ENTER,DOT);
+	u8 message_2[] = { "For user name Press 1" };
 	PrintTerminal(message_2,ENTER,DOT);
 
 	u8 input = 0;
@@ -66,12 +68,13 @@ int main()
 			break;
 		case '1':
 		{
-			GettingUserInputAndCheck(USERNAME);
-			u8 error=FALSE;
+			GettingUserInputAndCheck(user_name,USERNAME);
+			u8 error=NOK;
 			do
 			{
-				GettingUserInputAndCheck(PASSWORD);
-				error = GettingUserInputAndCheck(CONFIRM_PASSWORD);
+				GettingUserInputAndCheck(password_1,PASSWORD);
+				GettingUserInputAndCheck(password_2,CONFIRM_PASSWORD);
+				error=strcmp(password_1,password_2);
 				if (error)
 				{
 					u8 passwords_dont_match[]="Passwords don't match, Please try again";
@@ -79,11 +82,10 @@ int main()
 				}
 
 			}while(error);
-			u8 passwords_match[]="Passwords match Thankyou";
-			PrintTerminal(passwords_match, ENTER, DOT);
+			u8 passwords_match[]="Passwords match Thank you!";
+			PrintTerminal(passwords_match, ENTER, NO_DOT);
 			break;
 		}
-
 		default:
 			break;
 		}
@@ -107,25 +109,23 @@ void PrintTerminal(u8 *message,u8 enter,u8 dot)
 	}
 }
 
-u8 GettingUserInputAndCheck(u8 username_or_password)
+void GettingUserInputAndCheck(u8 data[],u8 Selector)
 {
-	u8 error=FALSE;
 	u8 index = 0;
 	u8 input = 0;
 	u8 user_input[100] = { 0 };
-	u8 user_name[100] = { 0 };
 
-	if(username_or_password==USERNAME)
+	if(Selector==USERNAME)
 	{
 		u8 username_text[] = { "UserName: " };
 		PrintTerminal(username_text, NO_ENTER,NO_DOT);
 	}
-	else if(username_or_password==PASSWORD)
+	else if(Selector==PASSWORD)
 	{
 		u8 password_text[] = { "Password: " };
 		PrintTerminal(password_text, NO_ENTER,NO_DOT);
 	}
-	else if(username_or_password==CONFIRM_PASSWORD)
+	else if(Selector==CONFIRM_PASSWORD)
 	{
 		u8 password_text[] = { "Confirm Password: " };
 		PrintTerminal(password_text, NO_ENTER,NO_DOT);
@@ -158,28 +158,7 @@ u8 GettingUserInputAndCheck(u8 username_or_password)
 	USART_voidSend(0X0D);
 
 	PrintTerminal(message, NO_ENTER,NO_DOT);
-	switch (username_or_password)
-	{
-	case USERNAME:
-		strcpy(user_name,user_input);
-		PrintTerminal(user_name, ENTER,DOT);
-		break;
-	case PASSWORD:
-		strcpy(password_1,user_input);
-		PrintTerminal(password_1, ENTER,DOT);
-		break;
-	case CONFIRM_PASSWORD:
-		strcpy(password_2,user_input);
-		PrintTerminal(password_2, ENTER,DOT);
-		break;
-	default:
-		/*do nothing*/
-		break;
-	}
 
-	if(strcmp(password_1,password_2))
-	{
-		error=TRUE;
-	}
-	return error;
+	strcpy(data,user_input);
+	PrintTerminal(data, ENTER, DOT);
 }
